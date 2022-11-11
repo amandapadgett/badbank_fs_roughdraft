a4695a4e46465bbc40c15222873541ef381a146e
anelisynagano <48553315+anelisynagano@users.noreply.github.com>
2022-11-11 18:13:53 +0000

    fix login and balance

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var  app = express();
+var app = express();
 var cors = require('cors');
 var dal = require('./dal.js');
 
@@ -8,59 +8,54 @@ app.use(cors());
 
 //create account
 app.post('/account/create/:name/:email/:password', function (req, res) {
-    dal.create(req.params.name, req.params.email, req.params.password)
-        .then((user) => {
-            console.log(user);
-            res.send(user);
-        });
+  dal
+    .create(req.params.name, req.params.email, req.params.password)
+    .then((user) => {
+      console.log(user);
+      res.send(user);
+    });
 });
 
 //login
 app.get('/account/login/:email/:password', (req, res) => {
-    dal.find(req.params.email)
-    .then((user) => {
-        if(user.length > 0) {
-            if(user[0].password === req.params.password) {
-                res.send(user[0])
-            } else {
-                res.send('Incorrect password');
-            }
-        }else {
-            res.send('Email not found');
-        }
-        
-    });
+  dal.findOne(req.params.email).then((user) => {
+    if (user) {
+      if (user.password === req.params.password) {
+        res.send(user);
+      } else {
+        res.send('Incorrect password');
+      }
+    } else {
+      res.send('Email not found');
+    }
+  });
 });
 
 //find one
 app.get('/account/findOne/:email', (req, res) => {
-    dal.find(req.params.email)
-    .then((user) => {
-        console.log(user);
-        res.send(user);
-    });
+  console.log(req.params);
+  dal.findOne(req.params.email).then((user) => {
+    console.log(user);
+    res.send(user);
+  });
 });
 
 //update after deposit or withdrawal
 app.get('/account/update/:email/:amount', (req, res) => {
-    var amount = Number(req.params.amount);
-    dal.update(req.params.email, amount)
-    .then((response) => {
-        console.log(response);
-        res.send(response);
-    });
+  var amount = Number(req.params.amount);
+  dal.update(req.params.email, amount).then((response) => {
+    console.log(response);
+    res.send(response);
+  });
 });
 
 //all accounts
 app.get('/account/all', (req, res) => {
-    dal.all().
-        then((docs) => {
-            console.log(docs);
-            res.send(docs)
-        });
+  dal.all().then((docs) => {
+    console.log(docs);
+    res.send(docs);
+  });
 });
-
-
 
 var port = 3000;
 app.listen(port);
--- public/balance.js
+++ public/balance.js
@@ -1,88 +1,91 @@
-function Balance(props){
-  const [show, setShow]     = React.useState(true);
-  const [status, setStatus] = React.useState('');  
+function Balance(props) {
+  const [show, setShow] = React.useState(true);
+  const [status, setStatus] = React.useState('');
   const [balance, setBalance] = React.useState('');
 
-  return  (
-      <Card
-        bgcolor="info"
-        header="Balance"
-        status={status}
-        body={ show ? (
+  const ctx = React.useContext(UserContext);
+  console.log(ctx);
+  return (
+    <Card
+      bgcolor='info'
+      header='Balance'
+      status={status}
+      body={
+        show ? (
           <>
-          <BalanceForm 
-            user={props.user}
-            setShow={setShow} 
-            setStatus={setStatus}
-            setBalance={setBalance}/> 
-            </> 
-          ) : ( 
-          <>
-          {' '}
-          <BalanceMsg 
-            setShow={setShow}
-            setStatus={setStatus} />
-          <h5>Your Balance Is: ${balance}</h5>
+            <BalanceForm
+              user={props.user}
+              setShow={setShow}
+              setStatus={setStatus}
+              setBalance={setBalance}
+            />
           </>
-          )
-        }
-      />
-  ) 
-}
-
-function BalanceMsg(props){
-  return(<>
-    <h5>Success</h5>
-    <button 
-      type="submit" 
-      className="btn btn-light" 
-      onClick={() => {
-        props.setShow(true)
-        props.setStatus('')
-      }} >
-        Recheck Balance
-    </button>
-  </>)
-}
-
-function BalanceForm(props){
-  const [email, setEmail]   = React.useState('');
-  const [balance, setBalance] = React.useState('');  
-  
-
-  function handle(){
-    fetch(`/account/findOne/${props.user.email}`)
-    .then(response => response.text())
-    .then(text => {
-      try {
-        const data = JSON.parse(text);
-        props.setStatus(text);
-        props.setShow(false);
-        setBalance(data.balance);
-        console.log('JSON: ', data);
-      } catch(err) {
-        props.setStatus(text)
-        console.log('err: ', text);
+        ) : (
+          <>
+            {' '}
+            <BalanceMsg setShow={setShow} setStatus={setStatus} />
+            <h5>Your Balance Is: ${balance}</h5>
+          </>
+        )
       }
-    })
+    />
+  );
+}
+
+function BalanceMsg(props) {
+  return (
+    <>
+      <h5>Success</h5>
+      <button
+        type='submit'
+        className='btn btn-light'
+        onClick={() => {
+          props.setShow(true);
+          props.setStatus('');
+        }}
+      >
+        Recheck Balance
+      </button>
+    </>
+  );
+}
+
+function BalanceForm(props) {
+  const [email, setEmail] = React.useState('');
+  const [balance, setBalance] = React.useState('');
+
+  function handle() {
+    fetch(`/account/findOne/${email}`)
+      .then((response) => response.text())
+      .then((text) => {
+        try {
+          const data = JSON.parse(text);
+          props.setStatus(text);
+          props.setShow(false);
+          setBalance(data.balance);
+          console.log('JSON: ', data);
+        } catch (err) {
+          props.setStatus(text);
+          console.log('err: ', text);
+        }
+      });
   }
 
-
-  return (<>
-
-    Email<br/>
-    <input type="input" 
-      className="form-control" 
-      placeholder="Enter email" 
-      value={props.user.email} 
-      onChange={e => setEmail(e.currentTarget.value)}
-      /><br/>
-
-    <button type="submit" 
-      className="btn btn-light" 
-      onClick={handle}>
+  return (
+    <>
+      Email
+      <br />
+      <input
+        type='input'
+        className='form-control'
+        placeholder='Enter email'
+        value={props.user.email}
+        onChange={(e) => setEmail(e.currentTarget.value)}
+      />
+      <br />
+      <button type='submit' className='btn btn-light' onClick={handle}>
         Check Balance
-    </button>
-
-  </>);
+      </button>
+    </>
+  );
 }
