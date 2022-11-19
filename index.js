98bc6f8fcd842c7bfe056dbd7f3cacd8f84725ca
amandapadgett <manda.padgett@gmail.com>
2022-11-18 22:22:33 -0500

    changed port

--- index.js
+++ index.js
@@ -72,5 +72,5 @@ app.get('/account/all', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`running on port: ${PORT}`));
